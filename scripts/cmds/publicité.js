1c2d892892477df93c1f1e484aec6bc9c82250c0
Jonhs-rudeus10 <dieumerciciribagula@gmail.com>
2024-10-13 11:42:43 +0200

    Update boss.js

--- scripts/cmds/publicité.js
+++ scripts/cmds/publicité.js
@@ -1,8 +1,8 @@
 module.exports = {
 	config: {
-			name: "publicité",
+			name: "boss",
 			version: "1.0",
-			author: "Shibai Otsutsuki",
+			author: "rudeus Ackerman",
 			countDown: 5,
 			role: 0,
 			shortDescription: "sarcasm",
@@ -15,6 +15,6 @@ onChat: async function({
 	message,
 	getLang
 }) {
-	if (event.body && event.body.toLowerCase() == "publicité") return message.reply("Salut! je me nomme Shibai Otsutsuki c'est moi le Dieu des Otsutsuki alias le Datara Naimo Kami Deidarabochi ☠️ dit G.A.L.V.A.T.R.O.N pour me contacter veillez m'écrire sur Whatsapp ( +2250594685427 ) où veillez m'appeler au ( +2250767868530 ) où encore m'écrire sur ma page Facebook: ( https://www.facebook.com/profile.php?id=61561751986955 ) merci!");
+	if (event.body && event.body.toLowerCase() == "publicité") return message.reply("voici l'uid de mon sempai ◈ ━━━━━━━━ ● ━━━━━━━━ ◈https://www.facebook.com/profile.php?id=61566739640941◈ ━━━━━━━━ ● ━━━━━━━━ ◈et son nom c'est◆━━━━━━━▣✦▣━━━━━━━━◆jonhs rudeus Smith◆━━━━━━━▣✦▣━━━━━━━━◆");
 }
 };
